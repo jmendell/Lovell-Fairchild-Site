ca670b2e55243d18a5df2246920ba8d668d87be0
Dennis Elder <dennis@sandovaldesign.com>
2017-07-31 16:59:03 -0700

    updating the news section hero background image positioning

--- wp-content/themes/lf/templates/section-hero-news.php
+++ wp-content/themes/lf/templates/section-hero-news.php
@@ -3,7 +3,7 @@ global $background;
 $id = get_the_ID();
 ?>
 
-<section class="hero">
+<section class="hero hero--news">
 	<?php get_template_part('templates/section', 'stars'); ?>
 	<?php //site_background_image($background); 
 	if( have_rows('background_image') ):
@@ -15,7 +15,7 @@ $id = get_the_ID();
 		$mobile = $background_image[0]['mobile_image']['url'];
 	?>
 
-		<figure class="background">
+		<figure class="background" style="background-position: center top">
 			<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/loader.png" class="responsive"
 				alt="<?php echo $alt_text; ?>" data-src='{"m":"<?php echo $mobile; ?>","d":"<?php echo $desktop; ?>"}'
 			/>

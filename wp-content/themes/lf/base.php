58e1a2b749ce5123c9f56ebcd0668ecf7d4ddd68
Dennis Elder <dnnsldr@Denniss-MacBook-Pro.local>
2017-09-07 11:09:48 -0700

    adding facebook comments

--- wp-content/themes/lf/base.php
+++ wp-content/themes/lf/base.php
@@ -9,6 +9,16 @@ use Roots\Sage\Wrapper;
 <html class="no-js" <?php language_attributes(); ?>>
   <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
+  <!-- facebook comments-->
+  <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=167343637152132";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<!-- end of facebook comments -->
     <!--[if lt IE 9]>
       <div class="alert alert-warning">
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
